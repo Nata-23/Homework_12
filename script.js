c31253d3f67b9a45bc22d32883ce4e34df7f3f37
Nataliia <d_tasha@ukr.net>
2022-04-01 14:48:10 +0200

    created a task

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 "use strict";
 //1
+const getGitHubUser = async (companyName) => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!response.ok) {
+      throw response;
+    }
+    const data = await response.json();
+    console.log("data", data);
+    // console.log(date.find((el) => el.company.name === companyName));
+  } catch (responseError) {
+    if (responseError.status === 404) {
+      console.log("User is not found");
+    } else {
+      console.error(error);
+    }
+  }
+};
+
+getGitHubUser("Johns Group");
